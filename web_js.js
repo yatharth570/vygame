e73030c6c35f9fb07de1c08aeaa867186dcd8c2c
yatharth570 <yatharth570@gmail.com>
2020-05-11 19:39:51 +0530

    Add files via upload

--- web_js.js
+++ web_js.js
@@ -1,10 +1,28 @@
 function myFunction(x) {
-    x.classList.toggle("change");
-    
+  x.classList.toggle("change");
+
 }
-$(document).ready(function(){
-  $('.menu').click(function(){
+$(document).ready(function () {
+  $('.menu').click(function () {
     $('ul').toggleClass('active');
     $('header').toggleClass('h_active');
   })
 })
+
+$(function () {
+  $('.slide').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 4,
+    arrows:false,
+    dots:true,
+    cssEase:'linear',
+    variableWidth: true,
+    variableHeight: true,
+    
+
+
+  });
+  
+  
+});
